a97f79da32dd81af2c3a781136657d6a838c231b
panthidasan980-zerox <panthidasan980@gmail.com>
2026-02-22 11:39:01 -0500

    Added Prints

--- project.py
+++ project.py
@@ -16,3 +16,15 @@ print("Day 10")
 print("Day 11")
 print("Day 12")
 print("Day 13")
+
+print("Day 14")
+print("Day 15") 
+print("Day 16")
+print("Day 17")
+print("Day 18")
+
+print("Day 19")
+print("Day 20")
+
+print("Day 21")
+print("Day 22")
